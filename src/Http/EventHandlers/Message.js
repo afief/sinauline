b38dc25a953d67b981389690d807fb001ebe4d25
Afief Yona Ramadhana <ramacyber@gmail.com>
2017-07-16 01:40:36 +0700

    Update message event handler

--- src/Http/EventHandlers/Message.js
+++ src/Http/EventHandlers/Message.js
@@ -1,4 +1,62 @@
+const _ = require('lodash')
+const { client } = require('../../Services/LineService')
+const QuestionsClass = require('../Repositories/Questions')
+const questions = new QuestionsClass()
+
+const sendQuestion = async function (replyToken) {
+  const q = await questions.getRandom()
+  console.log('RANDOMIZE QUESTION', JSON.stringify(q, null, 2))
+
+  if (q) {
+    const { question, choices } = questions
+
+    const templateActions = []
+    _.each(choices, (choice, i) => {
+      templateActions.push({
+        type: 'message',
+        label: _.trim(choice.text),
+        text: _.trim(choice.text)
+      })
+    })
+
+    let message = {
+      type: 'template',
+      altText: 'Sinauline, Pertanyaan baru!',
+      template: {
+        type: 'buttons',
+        text: question,
+        actions: templateActions
+      }
+    }
+    const result = await client.replyMessage(replyToken, message)
+    if (result) {
+      return true
+    }
+  }
+
+  return false
+}
+
+const sendException = async function (replyToken) {
+  const result = await client.replyMessage(replyToken, {
+    type: 'text',
+    text: 'Silakan balas \'HAI\' untuk mendapatkan pertanyaan selanjutnya!'
+  })
+  if (result) {
+    return true
+  }
+  return false
+}
+
 module.exports = async function (event) {
   console.log('MESSAGE', event)
-  return {}
+
+  const { replyToken } = event
+
+  if (event.message.type !== 'text' || event.message.text.toLowerCase() !== 'hai') {
+    sendException(replyToken)
+    return false
+  }
+
+  return sendQuestion(replyToken)
 }
