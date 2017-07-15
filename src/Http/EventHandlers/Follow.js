0c1159052bbe3304efaf48c0048dd4b3915f89f7
Afief Yona Ramadhana (128.199.74.45) <ramacyber@gmail.com>
2017-07-15 16:50:18 +0000

    update reply messages and function

--- src/Http/EventHandlers/Follow.js
+++ src/Http/EventHandlers/Follow.js
@@ -4,13 +4,19 @@ module.exports = async function (event) {
   console.log('FOLLOW', event)
 
   const welcomeMessages = [
-    'Selamat Datang di Sinauline!',
-    'Yuk mulai tebak-tebakan. Silakan balas \'HAI\' untuk mulai.'
+    {
+      type: 'text',
+      text: 'Selamat Datang di Sinauline!'
+    },
+    {
+      type: 'text',
+      text: 'Yuk mulai tebak-tebakan. Silakan balas \'HAI\' untuk mulai.'
+    }
   ]
   const { replyToken } = event
   let isSend = false
   try {
-    isSend = await client.pushMessage(replyToken, welcomeMessages)
+    isSend = await client.replyMessage(replyToken, welcomeMessages)
   } catch (e) {
     console.log('FOLLOW FAILEDREPLY', e.message)
   }
